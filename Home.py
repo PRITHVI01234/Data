e860966168939589a9d3097e6bfac752869f0e4d
PRITHVI01234 <143007555+PRITHVI01234@users.noreply.github.com>
2024-07-27 22:28:24 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,7 @@
-import streamlit as st
+import streamlit as st 
 import pandas as pd
+import os
+from Components import file_uploader
 from background import BackgroundCSSGenerator
 
 # Set up Streamlit page configuration
@@ -16,141 +18,42 @@ background_generator = BackgroundCSSGenerator(img1_path, img2_path)
 page_bg_img = background_generator.generate_background_css()
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
-# Hardcoded user credentials (in a real application, secure this properly)
-user_credentials = {
-    "CIT-CDC": "CIT2024@",
-}
+if not st.session_state.get('years'):
+  st.session_state.years = {}
 
-# Function to check login
-def check_login(username, password):
-    return user_credentials.get(username) == password
+st.set_page_config(page_title='TNEA Compare',layout="wide")
 
-# Authentication status stored in session state
-if 'authentication_status' not in st.session_state:
-    st.session_state.authentication_status = None
+st.title('Seat Matrix Comparison PDF Uploads')
 
-# Sign-in form
-if st.session_state.authentication_status != True:
-    st.title("Sign In")
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    if st.button("Sign In"):
-        if check_login(username, password):
-            st.session_state.authentication_status = True
-            st.session_state.username = username
-        else:
-            st.error("Invalid username or password")
-else:
-    st.title("TNEA 2023-2024 Seat Matrix Comparison")
-    st.divider()
+col1, col2 = st.columns(2)
 
-    # Read CSV files
-    form4_df = pd.read_csv('form4.csv')
-    form5_df = pd.read_csv('form5.csv')
+with col1:
+  file_uploader('Upload SeatMatrix **{}** th Year', 'a', 'N-1')
+with col2:
+  file_uploader('Upload SeatMatrix **{}** th Year', 'b', 'N')
 
-    # Define functions to filter data
-    def filter_form4(data):
-        filtered = data.copy()
-        bound = st.container(border=True)
-        left, check1, right = bound.columns([0.4, 0.135, 0.3])
-        
-        college_name = left.text_input('Enter College Name', placeholder='Enter College Name')
-        check1.write('')
-        check1.write('')
-        starts_with = check1.checkbox('Starts With', value=False)
-        college_code = int()
+def rename_first_four_columns(df):
 
-        try:
-            college_code = right.text_input('Enter College Code', placeholder='Enter College Code')
-            if college_code != '':
-                college_code = int(college_code)
-        except:
-            st.error('College Code is an Integer.')
+    new_column_names = ['College Code', 'College Name', 'Branch Code', 'Branch Name']
+    current_columns = df.columns.tolist()
+    rename_mapping = {current_columns[i]: new_column_names[i] for i in range(min(len(current_columns), 4))}
+    df = df.rename(columns=rename_mapping)
+    
+    return df
 
-        if college_name != '':
-            if starts_with:
-                filtered = filtered[filtered['College Name'].str.lower().str.startswith(college_name.lower())]
-            else:
-                filtered = filtered[filtered['College Name'].str.lower().str.contains(college_name.lower())]
+def execute_script():
 
-        if college_code:
-            filtered = filtered[filtered['College Code'].astype(str).str.startswith(str(college_code))]
+    df1 = pd.read_excel(r"Inputs/a.xlsx")
+    df2 = pd.read_excel(r"Inputs/b.xlsx")
+    df1 = rename_first_four_columns(df1)
+    df2 = rename_first_four_columns(df2)
+    df1.to_excel("Inputs/a.xlsx", index=False)
+    df2.to_excel("Inputs/b.xlsx", index=False)
 
-        return filtered
+    st.success("Changes have been made and files have been updated!")
 
-    def filter_form5(data):
-        filtered = data.copy()
-        bound = st.container(border=True)
-        _college_name, sw_toggle, _college_code = bound.columns([0.92, 0.3, 0.8])
 
-        college_name = _college_name.text_input('Enter College Name', placeholder='Enter College Name')
-        sw_toggle.write('')
-        sw_toggle.write('')
-        c_starts_with = sw_toggle.checkbox('Starts With', value=False)
-        college_code = int()
-
-        try:
-            college_code = _college_code.text_input('Enter College Code', placeholder='Enter College Code', value=1399)
-            if college_code != '':
-                college_code = int(college_code)
-        except:
-            st.error('College Code is an Integer.')
-
-        _branch_name, sw_toggle, _branch_code = bound.columns([0.92, 0.3, 0.8])
-
-        branch_name = _branch_name.text_input('Enter Branch Name', placeholder='Enter Branch Name')
-        sw_toggle.write('')
-        sw_toggle.write('')
-        b_starts_with = sw_toggle.checkbox('Starts With', value=False, key="yes")
-        branch_code = _branch_code.text_input('Enter Branch Code', placeholder='Enter Branch Code')
-
-        if college_name != '':
-            if c_starts_with:
-                filtered = filtered[filtered['College Name'].str.lower().str.startswith(college_name.lower())]
-            else:
-                filtered = filtered[filtered['College Name'].str.lower().str.contains(college_name.lower())]
-
-        if college_code:
-            filtered = filtered[filtered['College Code'].astype(str).str.startswith(str(college_code))]
-
-        if branch_name != '':
-            if b_starts_with:
-                filtered = filtered[filtered['Branch Name'].str.lower().str.startswith(branch_name.lower())]
-            else:
-                filtered = filtered[filtered['Branch Name'].str.lower().str.contains(branch_name.lower())]
-
-        if branch_code:
-            filtered = filtered[filtered['Branch Code'].astype(str).str.startswith(str(branch_code))]
-
-        return filtered
-
-    with st.sidebar:
-        st.session_state.form = st.selectbox(
-            label='Select Form',
-            options=['Form 4', 'Form 5']
-        )
-
-    if st.session_state.get('form'):
-        if st.session_state.form == 'Form 4':
-            st.session_state.filtered = filter_form4(form4_df)
-        elif st.session_state.form == 'Form 5':
-            st.session_state.filtered = filter_form5(form5_df)
-
-    if isinstance(st.session_state.get('filtered'), pd.DataFrame):
-        # Apply conditional formatting
-        def highlight_diff(row):
-            row_color = [''] * len(row)
-            if row['Difference'] > 0:
-                row_color = ['background-color: lightgreen'] * len(row)
-            elif row['Difference'] < 0:
-                row_color = ['background-color: red'] * len(row)
-            
-            if row['2023'] == 0:
-                row_color = ['background-color: green'] * len(row)
-            if row['2024'] == 0:
-                row_color = ['background-color: blue'] * len(row)
-
-            return row_color
-
-        styled_df = st.session_state.filtered.style.apply(highlight_diff, axis=1)
-        st.dataframe(styled_df, use_container_width=True)
+st.sidebar.subheader(":rainbow-background[Note:]")
+st.sidebar.success("**Upload both files, enter commit, and press 'Make Changes' to process.**")
+if st.sidebar.button('Make Changes'):
+    execute_script()
